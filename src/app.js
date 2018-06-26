a24efaf85b711402ef42199d5e3053e6badccada
ismumu <ismmm@qq.com>
2018-06-26 17:05:08 +0800

    add

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DatePicker } from 'antd';
+
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 
-import About from './about/index';
+
 
 import 'antd/dist/antd.css';
 import styles from './app.less';
@@ -13,13 +15,57 @@ import styles from './app.less';
 
 
 class App extends React.Component {
-    render () {
+
+    constructor() {
+        super();
+        this.state = {
+            collapsed: false,
+        }
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
         return (
-            <div>
-                <div className={styles.app}>welcome!</div>
-                <DatePicker />
-                <About />
-            </div>
+            <Layout className={styles.layout}>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className={styles.logo}>Run React With Webpack</div>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            className={styles.trigger}
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
         )
     }
 }
