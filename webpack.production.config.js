7f64585ca625457ff146f1188b657b40aa147381
ismumu <ismmm@qq.com>
2018-06-26 17:09:40 +0800

    add

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 
 const merge = require('webpack-merge');
 const common = require('./webpack.common.config.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = merge(common, {
     devtool: false,
